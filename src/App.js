ad513e78834eddbf936a9458fc058ec3118fd88f
Lipton Barua <liptonbarua274@gmail.com>
2022-09-30 17:46:38 +0600

    project-19

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import logo from './logo.svg';
 import './App.css';
 import Player from './Componernts/Player/Player';
 
+// toast.configure()
 function App() {
+
   return (
     <div className="App">
       <Player></Player>
--- src/Componernts/Card/Card.js
+++ src/Componernts/Card/Card.js
@@ -1,5 +1,7 @@
+
 import React, { useEffect, useState } from 'react';
 import './Card.css'
+
 const Card = (props) => {
     const{cart}=props;
     let total =0;
@@ -8,7 +10,7 @@ const Card = (props) => {
         total = total+product1.Time_required;
     }
     
-   
+
    const [server, setServer] = useState(0)
      
     useEffect(()=>{
